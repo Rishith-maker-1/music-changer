85cfbebfa56fbc57820ed7803d898c1a4907f10b
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-01-30 13:34:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 song="";
 songs="";
-function preload(){
-    song=loadSound("https://github.com/Rishith-maker-1/music-changer/blob/main/music.mp3?raw=true");
-    songs=loadSound("https://github.com/Rishith-maker-1/music-changer/blob/main/music2.mp3?raw=true");
-}
+
 function setup(){
-    canvas=createCanvas(600,500);
+    
+    canvas=createCanvas(500,350);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    
 }
 function draw(){
-    image(video,0,0,600,500);
+    image(video,0,0,500,350);
+}
+function preload(){
+    song=loadSound("music.mp3");
+    songs=loadSound("music2.mp3");
 }
