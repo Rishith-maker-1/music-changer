edb556bc3875d1e65bddf1828dd872a866ef619d
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-02-05 17:26:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+score=0;
+status1="";
+status2="";
 function setup(){
     canvas=createCanvas(500,350);
     canvas.center();
@@ -14,6 +17,18 @@ function setup(){
 }
 function draw(){
     image(video,0,0,500,350);
+    status1=song.isPlaying();
+    status2=songs.isPlaying();
+        stroke('black');
+        fill('red');
+    if(score>0.2){
+        circle(leftWristX,leftWristY,20);
+        songs.stop();
+        if(status2==false){
+            songs.play();
+            document.getElementById("name").innerHTML="Song - Peter Pan";
+        }
+    }
 }
 function modelLoaded(){
     console.log("model is loaded");
@@ -21,6 +36,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        score=results[0].pose.keypoints[9].score;
+        console.log(score);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("left wrist x = "+leftWristX+"left wrist y = "+leftWristY);
